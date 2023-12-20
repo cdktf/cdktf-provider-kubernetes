892f5e6e72ed7858018cbea28f3a23b97b007573
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2023-12-20 10:23:24 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/endpoints-v1/index.ts
+++ src/endpoints-v1/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/hashicorp/kubernetes/2.24.0/docs/resources/endpoints_v1
 // generated from terraform resource schema
 
