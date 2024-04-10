6caa8df475c3f5ad5007de46912d431dd1343c8f
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-04-10 03:15:03 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/data-kubernetes-persistent-volume-claim-v1/index.ts
+++ src/data-kubernetes-persistent-volume-claim-v1/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/hashicorp/kubernetes/2.28.1/docs/data-sources/persistent_volume_claim_v1
 // generated from terraform resource schema
 
--- src/resource-quota-v1/index.ts
+++ src/resource-quota-v1/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/hashicorp/kubernetes/2.28.1/docs/resources/resource_quota_v1
 // generated from terraform resource schema
 
