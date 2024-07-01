8833fd945e08ecddc11a9929eea079e84b89dfe0
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-07-01 08:23:35 +0000

    chore(deps): update  by cdktf/cdktf-repository-manager@60bef17

--- src/resource-quota/index.ts
+++ src/resource-quota/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/hashicorp/kubernetes/2.31.0/docs/resources/resource_quota
 // generated from terraform resource schema
 
