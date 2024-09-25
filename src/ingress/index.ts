ab640ed7d64a00fb2cfb03f846d2f5fea02e9b5d
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-09-25 10:22:11 +0000

    chore(deps): update  by cdktf/cdktf-repository-manager@3c86295

--- src/ingress/index.ts
+++ src/ingress/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/hashicorp/kubernetes/2.32.0/docs/resources/ingress
 // generated from terraform resource schema
 
