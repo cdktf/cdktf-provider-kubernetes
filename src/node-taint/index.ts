4281f7dc87f616807a840c3207ddf8bda01fd578
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2025-03-16 06:09:35 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/node-taint/index.ts
+++ src/node-taint/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/hashicorp/kubernetes/2.36.0/docs/resources/node_taint
 // generated from terraform resource schema
 
